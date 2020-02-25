a4a7fbe07937dfec4b94d99a614468694d8b3bbc
Ian Morelan <imorelan@campus-016-037.ucdavis.edu>
2020-02-25 11:59:26 -0800

    Added sample image to readme; adds multi-kingdom functionality

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
+---
+output:
+  pdf_document: default
+  html_document: default
+---
 # phyloseq_sankey
-A function that makes it easy to make interactive Sankey plots from phyloseq objects. Based on the hiervis package which is based on d3-hierarchy and d3-hiervis.
+A function that makes it super-duper easy to make interactive Sankey plots from phyloseq objects. Based on the hiervis package which is based on d3-hierarchy and d3-hiervis.
 
-## For a demonstration, check out the github pages website: https://ianartmor.github.io/phyloseq_sankey/
+These plots are admittedly pretty ugly, but I've found them to be useful for data exploration. Please feel to contribute / improve! It'd eventually be great to map colors onto these a la [metacoder](https://grunwaldlab.github.io/metacoder_documentation/). This may be easy to do, but I'm not very familiar with d3.
+
+### For a demonstration, check out the github pages website: https://ianartmor.github.io/phyloseq_sankey/
+
+<img src="docs/sample.png" align="center" width="700px"/>
--- plot_hierarchy.R
+++ plot_hierarchy.R
@@ -1,13 +1,11 @@
-
-
 plot_hierarchy <- function(physeq, number_of_taxa_to_plot=50, plot_type="sankey"){
-
+  
   library(phyloseq)
   library(tidyverse)
   library(hiervis)
-
-# start by defining abundances and top taxa functions. borrowed from microbiome package https://doi.org/doi:10.18129/B9.bioc.microbiome
- abundances <- function (x, transform = "identity") 
+  
+  # start by defining abundances and top taxa functions. borrowed from microbiome package https://doi.org/doi:10.18129/B9.bioc.microbiome
+  abundances <- function (x, transform = "identity") 
   {
     if (class(x) == "phyloseq") {
       otu <- as(otu_table(x), "matrix")
@@ -44,39 +42,41 @@ plot_hierarchy <- function(physeq, number_of_taxa_to_plot=50, plot_type="sankey"
     names(sort(rowSums(abundances(x)), decreasing = TRUE)[seq_len(n)])
   }
   
-# first, we prune to the specified number of taxa
-physeq_abund <-  prune_taxa(top_taxa(physeq, number_of_taxa_to_plot), physeq) 
-
-# then convert Kingdom-Genus tax table to dataframe
-tax_df <- as.data.frame(phyloseq::tax_table(physeq_abund)[,1:6])
-
-# add "path" column for each genus; this is used by hiervis to generate plot
-tax_df$path <- paste(tax_df$Kingdom, tax_df$Phylum, tax_df$Class, tax_df$Order, tax_df$Family, tax_df$Genus, sep = "/") 
-
-#populate data frame with taxa abundances
-tax_df$Abundance <- as.numeric(taxa_sums(physeq_abund))
-
-# subset to only include path and Abundance columns
-tax_df_subsetted <- tax_df[,c("path", "Abundance")]
-
-# "path" column also requires this hierarchical information
-k <- unique(paste(tax_df$Kingdom))
-kp <- unique(paste(tax_df$Kingdom, tax_df$Phylum,  sep = "/"))
-kpc <- unique(paste(tax_df$Kingdom, tax_df$Phylum, tax_df$Class,  sep = "/"))
-kpco  <- unique(paste(tax_df$Kingdom, tax_df$Phylum, tax_df$Class, tax_df$Order,  sep = "/"))
-kpcof  <- unique(paste(tax_df$Kingdom, tax_df$Phylum, tax_df$Class, tax_df$Order, tax_df$Family, sep = "/"))
-kpcofg   <- unique(paste(tax_df$Kingdom, tax_df$Phylum, tax_df$Class, tax_df$Order, tax_df$Family, tax_df$Genus, sep = "/"))
-
-#combines hierarchical info
-path_header <- c(k, kp, kpc, kpco, kpcof, kpcofg)
-
-# makes data frame from hierarchical info
-path_header_df <- data.frame(path_header, rep(NA, length(path_header)))
-colnames(path_header_df) <- c("path", "Abundance")
-
-# combine hierarchical info and abundance data frames
-df_to_plot <- rbind(path_header_df, tax_df_subsetted)
-
-return(hiervis( df_to_plot,  plot_type, nameField = "path", pathSep = "/", valueField = "Abundance", stat = "sum"))
+  # first, we prune to the specified number of taxa
+  physeq_abund <-  prune_taxa(top_taxa(physeq, number_of_taxa_to_plot), physeq) 
+  
+  # then convert Kingdom-Genus tax table to dataframe
+  tax_df <- as.data.frame(phyloseq::tax_table(physeq_abund)[,1:6])
+  
+  tax_df$Life_type <- rep("Cellular", nrow(tax_df))
+  
+  # add "path" column for each genus; this is used by hiervis to generate plot
+  tax_df$path <- paste(tax_df$Life_type, tax_df$Kingdom, tax_df$Phylum, tax_df$Class, tax_df$Order, tax_df$Family, tax_df$Genus, sep = "/") 
+  
+  #populate data frame with taxa abundances
+  tax_df$Abundance <- as.numeric(taxa_sums(physeq_abund))
+  
+  # subset to only include path and Abundance columns
+  tax_df_subsetted <- tax_df[,c("path", "Abundance")]
+  
+  # "path" column also requires this hierarchical information
+  life_type <- "Cellular"
+  k <- unique(paste(tax_df$Life_type, tax_df$Kingdom, sep = "/"))
+  kp <- unique(paste(tax_df$Life_type,tax_df$Kingdom, tax_df$Phylum,  sep = "/"))
+  kpc <- unique(paste(tax_df$Life_type,tax_df$Kingdom, tax_df$Phylum, tax_df$Class,  sep = "/"))
+  kpco  <- unique(paste(tax_df$Life_type,tax_df$Kingdom, tax_df$Phylum, tax_df$Class, tax_df$Order,  sep = "/"))
+  kpcof  <- unique(paste(tax_df$Life_type,tax_df$Kingdom, tax_df$Phylum, tax_df$Class, tax_df$Order, tax_df$Family, sep = "/"))
+  kpcofg   <- unique(paste(tax_df$Life_type,tax_df$Kingdom, tax_df$Phylum, tax_df$Class, tax_df$Order, tax_df$Family, tax_df$Genus, sep = "/"))
+  
+  #combines hierarchical info
+  path_header <- c(life_type, k, kp, kpc, kpco, kpcof, kpcofg)
+  
+  # makes data frame from hierarchical info
+  path_header_df <- data.frame(path_header, rep(NA, length(path_header)))
+  colnames(path_header_df) <- c("path", "Abundance")
+  
+  # combine hierarchical info and abundance data frames
+  df_to_plot <- rbind(path_header_df, tax_df_subsetted)
+  
+  return(hiervis( df_to_plot,  plot_type, nameField = "path", pathSep = "/", valueField = "Abundance", stat = "sum"))
 }
-
